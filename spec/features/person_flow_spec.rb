d001463fb9e5c757c4e17b80b03f6b47dc852efc
Steve Polito <info@stevepolitodesign.com>
2019-11-04 20:40:53 -0500

    write pending person flow specs

---
+++ spec/features/person_flow_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "PersonFlows", type: :feature do
+  describe "index page" do
+    it "lists all persons" do
+      skip
+    end
+  end
+  describe "show page" do
+    it "diplays person's name and addresses" do
+      skip
+    end
+  end
+  describe "edit page" do
+    it "diplays a form to edit the person" do
+      skip
+    end
+    it "displays a link to delete the person" do
+      skip
+    end
+  end
+  describe "new page" do
+    it "displays a form to create a new person" do
+      skip
+    end
+  end
+  describe "form errors" do
+    it "displays erros if the form is not valid" do
+      skip
+    end
+  end
+end
