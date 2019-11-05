4f9c61d170e012405b21b3fbe94750e4446bb425
Steve Polito <info@stevepolitodesign.com>
2019-11-05 12:51:16 -0500

    update people factory, and associate person with address.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ApplicationRecord
+    has_many :addresses, dependent: :destroy
     validates :first_name, presence: true
     validates :last_name, presence: true
 end
--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -2,5 +2,16 @@ FactoryBot.define do
   factory :person do
     sequence(:first_name) { |n| "First Name #{n}"}
     sequence(:last_name) { |n| "Last Name #{n}"}
+
+    factory :person_with_addresses do
+      transient do
+        addresses_count { 5 }
+      end
+
+      after(:create) do |person, evaluator|
+        create_list(:address, evaluator.addresses_count, person: person)
+      end
+    end
+
   end
 end
