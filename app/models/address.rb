d2c3d2effcf6c9c60ba5ddff18b2e058c23fe313
Steve Polito <info@stevepolitodesign.com>
2019-11-05 12:54:51 -0500

    write address specs

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :person
+  validates :kind, presence: true
+  validates :street, presence: true
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    let!(:address) { FactoryBot.create(:address) }
+    it "can be created" do
+      expect(address).to be_valid
+    end
+  end
+  describe "validations" do
+    let(:address) { FactoryBot.build(:address) }
+    it "should have a kind" do
+      address.kind = nil
+      expect(address).to_not be_valid
+    end
+    it "should have a street" do
+      address.street = nil
+      expect(address).to_not be_valid
+    end
+    it "should have a person" do
+      address.person = nil
+      expect(address).to_not be_valid
+    end 
+  end
 end
