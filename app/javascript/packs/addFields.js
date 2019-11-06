05d0a3015c598eb8e6d123b28016993314222cf9
Steve Polito <info@stevepolitodesign.com>
2019-11-06 08:09:01 -0500

    create blank javascript to handle adding fields

---
+++ app/javascript/packs/addFields.js
@@ -0,0 +1,12 @@
+
+
+class addFields {
+    constructor(){
+        this.links = document.querySelectorAll('.add_fields');
+        console.log(this.links);  
+    }
+}
+
+const add_fields = new addFields();
+
+
