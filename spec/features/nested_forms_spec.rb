a43ca62dadcf52c97b92a7115b7973cde2521e25
Steve Polito <info@stevepolitodesign.com>
2019-11-07 16:20:05 -0500

    create pending spec

---
+++ spec/features/nested_forms_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "NestedForms", type: :feature do
+  describe "nested address form", js: true do
+    it "allows a user to add multiple addresses" do
+      skip
+    end
+    it "does not save an empty address" do
+      skip
+    end
+    it "allows a user to delete multiple addresses" do
+      skip
+    end
+  end
+end
