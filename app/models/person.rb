768156c0b66c22cc615a23d807cfcba2f9ba5e6d
Steve Polito <info@stevepolitodesign.com>
2019-11-10 17:57:12 -0500

    update model to reflect documentation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ApplicationRecord
-    has_many :addresses, dependent: :destroy
+    has_many :addresses, inverse_of: :person
     accepts_nested_attributes_for :addresses, allow_destroy: true, reject_if: :all_blank
     validates :first_name, presence: true
     validates :last_name, presence: true
